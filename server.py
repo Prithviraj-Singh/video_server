97a787d65e25eb957f4fcdfd7fb662373535b6fe
Prithviraj-Singh <72137830+Prithviraj-Singh@users.noreply.github.com>
2021-06-15 08:20:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ vidsock = list(range(100))
 sendsock = list(range(100))
 threadrec = list(range(100))
 threadcreate = list(range(100))
+vidadd = list(range(100))
 rec = list(range(100))
 encoded = numpy.zeros((200, 200, 3))
 shut = list(range(100))
@@ -45,61 +46,61 @@ def create_img(i):
     while (True):
         try:
             if i == 0:
-                final[:300, :300] = decimg[i]
+                final[:150, :150] = decimg[i]
             if i == 1:
-                final[:300, 300:600] = decimg[i]
+                final[:150, 150:300] = decimg[i]
             if i == 2:
-                final[300:600, :300] = decimg[i]
+                final[150:300, :150] = decimg[i]
             if i == 3:
-                final[300:600, 300:600] = decimg[i]
+                final[150:300, 150:300] = decimg[i]
         except cv2.error:
             create_img(i)
         except TypeError:
             pass
 
 
-def send(conn, i):
+def send(conn,i,address):
     global final
     try:
+        print("sending")
         while (True):
             encoded = cv2.imencode(".jpg", final)[1].tobytes()
-            conn.send(encoded)
+            conn.sendto(encoded,address)
     except socket.error:
         conn.close()
 
 
 def startsocket(count, conn):
-    vidsock[count] = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    vidsock[count] = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     vidsock[count].bind((myip, 0))
-    vidsock[count].listen(1)
+
 
     sendsock[count] = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     sendsock[count].bind((myip, 0))
 
+
     conn.send(str(vidsock[count].getsockname()[1]).encode())
     print("er")
-    vidconn, address = vidsock[count].accept()
+    data,vidadd[count] = vidsock[count].recvfrom(100)
     print("er")
     conn.send(str(sendsock[count].getsockname()[1]).encode())
     print("er")
+
 
     threadrec[count] = threading.Thread(target=receive, args=(sendsock[count], count,))
     threadrec[count].start()
     threadcreate[count] = threading.Thread(target=create_img, args=(count,))
     threadcreate[count].start()
-    threadsnd[count] = threading.Thread(target=send, args=(vidconn, count,))
+    threadsnd[count] = threading.Thread(target=send, args=(vidsock[count],count,vidadd[count]))
     threadsnd[count].start()
 
 
 def forall(count):
     sockall = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    sockall.bind((myip, 888))
+    sockall.bind((myip, 8888))
     sockall.listen(100)
     while (True):
         conn[count], address = sockall.accept()
         startsocket(count, conn[count])
         count = count + 1
 
-
-threadall = threading.Thread(target=forall, args=(0,))
-threadall.start()
